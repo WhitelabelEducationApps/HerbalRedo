b8c8c66f1a66285d0c4b61083b4db79d450c503d
rsavutiu <rsavutiu@gmail.com>
2026-05-07 14:40:29 +0300

    Updated platform whitelabel module

--- shared/src/commonMain/kotlin/com/herbal/di/KoinInitializer.kt
+++ shared/src/commonMain/kotlin/com/herbal/di/KoinInitializer.kt
@@ -1,6 +1,8 @@
 package com.herbal.di
 
+import com.whitelabel.platform.di.commonModule
 import com.whitelabel.platform.di.platformModule as whitelabelPlatformModule
+import com.whitelabel.platform.di.viewModelModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
